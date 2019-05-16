33ba6f206827943a52d6e3a47b5a287b2f65c11f
FilaCo <filaco159@yandex.ru>
2019-05-16 15:45:14 +0500

    Added an inteface for building bodies

---
+++ ImpLite/Bodies/IRigidBodyBuilder.cs
@@ -0,0 +1,21 @@
+namespace ImpLite.Bodies
+{
+    public interface IRigidBodyBuilder
+    {
+        void Reset();
+
+        void SetMaterial(float density, float restitution, float staticFriction, float dynamicFriction);
+
+        void SetPosition(Vector2 position);
+
+        void SetMask(ushort maskBits, ushort categoryBits);
+
+        void SetBox(float width, float height);
+
+        void ComputeMass();
+
+        void SetStatic(bool flag);
+
+        void SetKinematic(bool flag);
+    }
+}
