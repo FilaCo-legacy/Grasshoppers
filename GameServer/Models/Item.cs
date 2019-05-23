d00a653adcb12d6e8e9b935823ca443028026535
FilaCo <FilaCo159@yandex.ru>
2019-05-24 04:26:34 +0500

    Added Item class

---
+++ GameServer/Models/Item.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameServer.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+    }
+}
