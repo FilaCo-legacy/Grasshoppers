942b2895187d1de43b6db06968908e486ca3312a
FilaCo <FilaCo159@yandex.ru>
2019-05-24 10:09:53 +0500

    Changed project template

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
-namespace GameServer
+﻿namespace Tmp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
