7c3c7ce0cfc43ef2a30a67b5e5a946d6d94d0127
FilaCo <FilaCo159@yandex.ru>
2019-05-26 00:00:43 +0500

    Added comments

--- GameServer/DBModels/Player.cs
+++ GameServer/DBModels/Player.cs
@@ -1,24 +1,40 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameServer.DBModels
 {
+    /// <summary>
+    /// Represents the player entry in database
+    /// </summary>
     public class Player
     {
         public int Id { get; set; }
         
+        /// <summary>
+        /// A name of this <see cref="Player"/>
+        /// </summary>
+        [Required, MaxLength(50)]
         public string Name { get; set; }
         
+        /// <summary>
+        /// A collection of <see cref="Item"/>s that this <see cref="Player"/>'s inventory contains
+        /// </summary>
         public ICollection<Item> Items { get; set; }
         
+        /// <summary>
+        /// A collection of <see cref="GameSession"/>s that this <see cref="Player"/> participated in 
+        /// </summary>
         public ICollection<GameSession> GameSessions { get; set; }
         
-        public PlayerAppearance PlayerAppearance { get; set; }
+        /// <summary>
+        /// An appearance of this <see cref="Player"/>
+        /// </summary>
+        [Required]
+        public PlayerAppearance Appearance { get; set; }
 
         public Player()
         {
-            PlayerAppearance = new PlayerAppearance();
+            Appearance = new PlayerAppearance();
             Items = new List<Item>();
             GameSessions = new List<GameSession>();
         }
