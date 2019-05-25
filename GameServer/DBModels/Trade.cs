0406e2807526e67660fafcd101bbca7d62de564b
FilaCo <FilaCo159@yandex.ru>
2019-05-26 00:14:32 +0500

    Added comments for the Trade class

--- GameServer/DBModels/Trade.cs
+++ GameServer/DBModels/Trade.cs
@@ -6,14 +6,26 @@ using Newtonsoft.Json;
 
 namespace GameServer.DBModels
 {
+    /// <summary>
+    /// Class that stores the information about two <see cref="Player"/>'s trade
+    /// </summary>
     public class Trade
     {
+        /// <summary>
+        /// Serialized collection of the first <see cref="Player"/> <see cref="Item"/>s
+        /// </summary>
         internal string _firstPlayerItems { get; private set; }
 
+        /// <summary>
+        /// Serialized collection of the second <see cref="Player"/> <see cref="Item"/>s
+        /// </summary>
         internal string _secondPlayerItems { get; private set; }
         
         public int Id { get; set; }
         
+        /// <summary>
+        /// A date when this <see cref="Trade"/> happened
+        /// </summary>
         [Required]
         public DateTime Date { get; set; }
         
@@ -27,6 +39,9 @@ namespace GameServer.DBModels
         [ForeignKey("SecondPlayerId")]
         public Player SecondPlayer { get; set; }
 
+        /// <summary>
+        /// Deserialized collection of the first <see cref="Player"/> <see cref="Item"/>s
+        /// </summary>
         [NotMapped]
         public ICollection<Item> FirstPlayerItems
         {
@@ -34,6 +49,9 @@ namespace GameServer.DBModels
             set => _firstPlayerItems = value.ToString();
         }
 
+        /// <summary>
+        /// Deserialized collection of the second <see cref="Player"/> <see cref="Item"/>s
+        /// </summary>
         [NotMapped]
         public ICollection<Item> SecondPlayerItems
         {
