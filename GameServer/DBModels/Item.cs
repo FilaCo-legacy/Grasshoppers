eb80900e894688980856da9c86178076296e7a2e
FilaCo <FilaCo159@yandex.ru>
2019-05-26 00:10:44 +0500

    Added comments for the Item class

--- GameServer/DBModels/Item.cs
+++ GameServer/DBModels/Item.cs
@@ -4,6 +4,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameServer.DBModels
 {
+    /// <summary>
+    /// Enum to discrete <see cref="Item"/>'s rarity
+    /// </summary>
     public enum ItemRarity
     {
         Common,
@@ -13,16 +16,28 @@ namespace GameServer.DBModels
         Legendary
     }
     
+    /// <summary>
+    /// Class that represents an item in some <see cref="Player"/>'s inventory
+    /// </summary>
     public class Item
     {
         public int Id { get; set; }
         
+        /// <summary>
+        /// A name of this <see cref="Item"/>
+        /// </summary>
         [Required]
         public string Name { get; set; }
         
+        /// <summary>
+        /// A rarity rate for this <see cref="Item"/>
+        /// </summary>
         [Required]
         public ItemRarity Rarity { get; set; }
         
+        /// <summary>
+        /// A collection of <see cref="Player"/>s which have this <see cref="Item"/> in their inventory
+        /// </summary>
         public ICollection<Player> Players { get; set; }
 
         public Item()
