ecdb808e1ce626b151774b54ba577948f022aad6
FilaCo <FilaCo159@yandex.ru>
2019-06-06 14:00:54 +0500

    Added necessary attribs for Player class and Many to many part for it

--- WebApp/Models/Player.cs
+++ WebApp/Models/Player.cs
@@ -1,11 +1,23 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApp.Models
 {
     public class Player
     {
         public int Id { get; set; }
         
+        [Required, MaxLength(50)]
         public string Name { get; set; }
         
+        [Required]
         public string SpritePath { get; set; }
+        
+        public ICollection<Item> Items { get; set; }
+
+        public Player()
+        {
+            Items = new List<Item>();
+        }
     }
 }
