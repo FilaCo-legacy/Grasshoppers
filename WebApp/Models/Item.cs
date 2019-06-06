c19a65788c324980a59bbf64935482e20a8da3be
FilaCo <FilaCo159@yandex.ru>
2019-06-06 13:59:42 +0500

    Added necessary attributes to Item class and added relation with Players: many to many

--- WebApp/Models/Item.cs
+++ WebApp/Models/Item.cs
@@ -1,13 +1,26 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApp.Models
 {
     public class Item
     {
         public int Id { get; set; }
         
+        [Required, MaxLength(50)]
         public string Name { get; set; }
         
+        [Required]
         public ItemRarity Rarity { get; set; }
         
+        [Required]
         public string SpritePath { get; set; }
+        
+        public ICollection<Player> Players { get; set; }
+
+        public Item()
+        {
+            Players = new List<Player>();
+        }
     }
 }
