8ee6fcea36275bac95e35c92db6337d96754eae7
FilaCo <FilaCo159@yandex.ru>
2019-06-08 22:32:38 +0500

    Added all dbsets in platformer context class

--- WebApp/Models/PlatformerContext.cs
+++ WebApp/Models/PlatformerContext.cs
@@ -4,12 +4,26 @@ namespace WebApp.Models
 {
     public sealed class PlatformerContext : DbContext
     {
-        public DbSet<InventoryEntry> ItemsPlayers { get; set; }
+        public DbSet<InventoryEntry> Inventories { get; set; }
         
         public DbSet <Item> Items { get; set; }
         
-        public DbSet <Character> Players { get; set; }
-
+        public DbSet <Character> Characters { get; set; }
+        
+        public DbSet<GameSession> GameSessions { get; set; }
+        
+        public DbSet<Mission> Missions { get; set; }
+        
+        public DbSet<Map> Maps { get; set; }
+        
+        public DbSet<CharacterResultEntry> CharactersResults { get; set; }
+        
+        public PlatformerContext(DbContextOptions options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new ItemConfiguration());
@@ -18,12 +32,6 @@ namespace WebApp.Models
             modelBuilder.ApplyConfiguration(new CharacterResultEntryConfiguration());
             
             base.OnModelCreating(modelBuilder);
-        }
-        
-        public PlatformerContext(DbContextOptions options)
-            : base(options)
-        {
-            Database.EnsureCreated();
         }
     }
 }
